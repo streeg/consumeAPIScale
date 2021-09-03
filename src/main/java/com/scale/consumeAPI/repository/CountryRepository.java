d3c9340c3339b9112249bfc71c4c8ad9401b0b4a
streeg <140141961@aluno.unb.br>
2021-09-03 11:20:46 -0300

    removing country files

--- src/main/java/com/scale/consumeAPI/repository/CountryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.scale.consumeAPI.repository;
-
-import com.scale.consumeAPI.model.Country;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
